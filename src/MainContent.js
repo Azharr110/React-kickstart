adc2d379089d42709951cf6ade8c2094670d3f8a
Azhar <abdazhar786@gmail.com>
2023-03-06 23:54:22 +0530

    Add MainContent

---
+++ src/MainContent.js
@@ -0,0 +1,18 @@
+//18- Organize components
+import React from 'react';
+import './index.css'
+
+
+export default function MainContent(){
+    return(
+      <div>
+        <h1>Reasons why I'm excited to Learning react</h1>
+      <ol>
+        <li>It's popular library, so I'll be able to fit in with the cool kids!</li>
+        <li>I'm more likely to get the job as a developer if I know React</li>
+        
+        
+      </ol>
+      </div>
+    )
+  }
