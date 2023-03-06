c0acf50af32e6435e440777ca395b0c82f8f82a2
Azhar <abdazhar786@gmail.com>
2023-03-06 23:53:35 +0530

    Add foooter

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+//18- Organize components
+import React from 'react';
+import './index.css'
+
+
+
+export default function Footer(){
+    return(
+      <footer className='footer'>
+        <small>© 2023 Azh Devlopment. All rights reserved</small> 
+        {/* alt+ 0169 */}
+      </footer>
+    )
+  }
