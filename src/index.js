89c9bf5bb4a13913366d94e4216886841e587324
Azhar <abdazhar786@gmail.com>
2023-03-07 00:09:30 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// //1-
+// ReactDOM.render(<ul>
+//     <li>Thing 1</li>
+//     <li>Thing 2</li>
+// </ul> , document.getElementById("root")
+// )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//2
+
+// function MainContent(){
+//     return (
+//     <h1>I'm Learning React!</h1>
+//     )
+// }
+
+// ReactDOM.render(
+//     <div>
+//         <MainContent/>
+//     </div>, document.getElementById("root")
+// )
+
+//Build React info site
+//Challenge
+
+//1
+// ReactDOM.render(
+//     <div>
+//         <h1>Azhar Nation</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </div>
+// , document.getElementById("root"))
+
+//
+
+
+const navbar = (
+    <nav>
+        <h1>Azhar Nation</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+console.log(ReactDOM)
+ReactDOM.render(navbar, document.getElementById("root"))
+
+// import React from "react"
+// import ReactDOM from "react-dom/client";
+
+//  const page = (
+//   <h1>Helllll</h1>
+//  )
+//  ReactDOM.render(page, document.getElementById("root"))
